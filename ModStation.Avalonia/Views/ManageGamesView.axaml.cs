fbbd7392a15724826be53f89d189a45729c624de
Israel Albino Galvan <israel.galvan@live.com>
2025-01-06 20:00:49 -0300

    Feat double click to open mod manager

--- ModStation.Avalonia/Views/ManageGamesView.axaml.cs
+++ ModStation.Avalonia/Views/ManageGamesView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using ModManager.Core.Entities;
 using ModStation.Avalonia.ViewModels;
 
@@ -9,5 +11,16 @@ public partial class ManageGamesView : UserControl
     public ManageGamesView()
     {
         InitializeComponent();
+    }
+
+    private void ListBoxItem_MouseDoubleClick(object sender, PointerPressedEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            var grid = sender as Grid;
+            var game = grid?.DataContext as Game;
+            var dataContext = DataContext as ManageGamesViewModel;
+            dataContext?.OpenManageMods(game);
+        }
     }
 }
